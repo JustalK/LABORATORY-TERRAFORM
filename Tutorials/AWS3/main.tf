0ee76242d8c5b152cf7df0e0e94a2a111afdff2a
justalk <justal.kevin@gmail.com>
2022-07-21 21:25:10 +0200

    [FEATURE] Testing to add volume

---
+++ Tutorials/AWS3/main.tf
@@ -0,0 +1,59 @@
+terraform {
+
+  cloud {
+    organization = "Justal"
+
+    workspaces {
+      name = "tutorial-aws-terraform"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_ebs_volume" "example" {
+  availability_zone = "us-west-2c"
+  size              = 1
+}
+
+resource "aws_volume_attachment" "ebs_att" {
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.example.id
+  instance_id = aws_instance.app_server.id
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.instance_name
+  }
+}
