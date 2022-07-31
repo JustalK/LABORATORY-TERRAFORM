b8a170d7d9bf611dada0492b1802c24f64ef916c
justalk <justal.kevin@gmail.com>
2022-07-31 11:11:23 +0200

    [FEATURE] - Creating a rds table

---
+++ Tutorials/RDS/RDS1/variables.tf
@@ -0,0 +1,10 @@
+variable "region" {
+  default     = "us-east-2"
+  description = "AWS region"
+}
+
+variable "db_password" {
+  description = "RDS root user password"
+  sensitive   = true
+  default = "education123"
+}
